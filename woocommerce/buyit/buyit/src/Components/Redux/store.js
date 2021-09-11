9109305219014a7e1d2964baef5ae5992ba5fb44
Gopi Krishna <krishsyv@gmail.com>
2021-09-11 15:48:50 +0530

    create react app for clothing and add files for home page

--- woocommerce/buyit/buyit/src/Components/Redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import ProductReducer from "./ProductReducer";
-import thunk from 'redux-thunk';
-
-const store = new createStore(ProductReducer, applyMiddleware(thunk));
-
-export default store;
